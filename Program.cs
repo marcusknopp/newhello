ee922ae25cb20e172097805256f416fb5f0c8f6d
Marcus Knopp <marcusknopp@hotmail.com>
2025-06-10 14:30:18 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AudioProgram
+{
+    // Define the AUDIO class
+    class AUDIO
+    {
+        public int Tone { get; set; }
+        public int Time { get; set; }
+
+        // Constructor
+        public AUDIO(int tone, int time)
+        {
+            Tone = tone;
+            Time = time;
+        }
+
+        // Method to play the sound
+        public void PlaySound()
+        {
+            Console.Beep(Tone, Time);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, welcome to my AI generated program.");
+            Console.Write("Enter the frequency (in Hz): ");
+            int tone = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the duration (in milliseconds): ");
+            int time = int.Parse(Console.ReadLine());
+
+            AUDIO audio = new AUDIO(tone, time);
+            audio.PlaySound();
+
+            Console.WriteLine("Sound played successfully!");
+        }
+    }
+}
